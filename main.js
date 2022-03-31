7d74409242116cb4cf081c2b691e782f856c4119
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-31 23:54:30 +0200

    Non ho ancora ben capito argomento

---
+++ main.js
@@ -0,0 +1,27 @@
+// Descrizione
+// Partendo da un array di immagini, realizziamo uno slider con Vue.js come da mockup
+// Al click delle freccette "sinistra" e "destra" lo slider cambierà l'immagine visibile passando alla successiva oppure alla precedente.
+// Consigli
+// Ricordiamoci come visualizzare un singolo elemento alla volta di un array
+// Attenzione
+// Quando le immagini terminano, lo slider ricomincerà dalla prima
+// Bonus
+// applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente ( riguardare la documentazione su i lifecycle hooks )
+// Al click su un pallino verrà visualizzata l'immagine di riferimento associata ( il terzo pallino sarà associato alla terza immagine e così via..)
+
+var app = new Vue(
+    {
+        el: "#banana",
+        data: {
+            immagggini:[
+                'https://source.unsplash.com/random/?shoes',
+                'https://source.unsplash.com/random/?fruits',
+                'https://source.unsplash.com/random/?city',
+                'https://source.unsplash.com/random'
+            ],
+
+            
+        },
+
+    },
+);
